0437976befa361d7d1ee7b0519a7fba39a9578f8
sab sureka <sab.sureka@gmail.com>
2018-05-24 03:07:31 +0000

    Checking hello into github

---
+++ hello.rb
@@ -0,0 +1,16 @@
+puts "Hello World!"
+
+  gift = [
+          "half of a leftover burrito",
+          "a day-old cup of coffee",
+          "a tesla",
+          "a life-size quadrocopter",
+          "a slice of stale wedding cake",
+          "a medium-sized island off the coast of Sumatra",
+          "a visual prosthesis",
+         ]
+  puts "All I got for valentine's day was #{gift.sample}."
+  puts "All I got for valentine's day was #{gift.sample}."
+  
+  
+  
